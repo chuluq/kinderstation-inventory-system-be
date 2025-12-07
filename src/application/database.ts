06a2a987511eadd2b9eba61bafd1abe241c95a7d
Chuluq <choirulchuluq10@gmail.com>
2025-12-07 21:54:57 +0700

    feat: add log to prisma

--- src/application/database.ts
+++ src/application/database.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../generated/prisma/client";
+import { logger } from "./logging";
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST as string,
@@ -9,6 +10,43 @@ const adapter = new PrismaMariaDb({
   database: process.env.DATABASE_NAME as string,
   connectionLimit: 5,
 });
-const prisma = new PrismaClient({ adapter });
+
+const prisma = new PrismaClient({
+  adapter,
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "event",
+      level: "error",
+    },
+    {
+      emit: "event",
+      level: "info",
+    },
+    {
+      emit: "event",
+      level: "warn",
+    },
+  ],
+});
+
+prisma.$on("query", (e) => {
+  logger.info(e);
+});
+
+prisma.$on("error", (e) => {
+  logger.error(e);
+});
+
+prisma.$on("info", (e) => {
+  logger.info(e);
+});
+
+prisma.$on("warn", (e) => {
+  logger.warn(e);
+});
 
 export { prisma };
