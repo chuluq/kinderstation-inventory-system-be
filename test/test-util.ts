1361dbb8d75135aa7c989197c478c20195c8ccce
Chuluq <choirulchuluq10@gmail.com>
2025-12-21 19:41:41 +0700

    fix: delete user

--- test/test-util.ts
+++ test/test-util.ts
@@ -2,7 +2,7 @@ import { prisma } from "../src/application/database.js";
 
 export class UserTest {
   static async delete() {
-    await prisma.user.delete({
+    await prisma.user.deleteMany({
       where: {
         email: "user@test.com",
       },
